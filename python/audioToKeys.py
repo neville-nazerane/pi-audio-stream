e8c2a83b8d2c52700a768aed8ad9fdf664d1c8f6
Neville <live.neville@live.com>
2023-12-27 03:39:04 -0700

    record key words

--- python/audioToKeys.py
+++ python/audioToKeys.py
@@ -2,7 +2,7 @@ import pyaudio
 import requests
 import sys
 import uuid
-
+import threading
 
 # Settings
 SERVER_URL = f"http://{sys.argv[1]}"
@@ -14,26 +14,47 @@ RATE = 44100
 CHUNK = 1024
 
 
-# Initialize PyAudio
+
+
+def record_audio(stop_event):
+    frames = []
+    while not stop_event.is_set():
+        data = stream.read(CHUNK, exception_on_overflow=False)
+        frames.append(data)
+    return frames
+
+
+
+
+
 audio = pyaudio.PyAudio()
 
-# Open stream
+input("Hit enter to begin")
+
 stream = audio.open(format=FORMAT, channels=CHANNELS,
                     rate=RATE, input=True,
                     frames_per_buffer=CHUNK)
-
-
-
 with open("keywords.txt", "r") as file:
     for line in file:
+
+        stop_event = threading.Event()
+        recorder_thread = threading.Thread(target=record_audio, args=(stop_event,))
+        recorder_thread.start()        
+
         input(line.strip())
-        # input("Press Enter to ...")
-        # requests.post(f'{SERVER_URL}/audioToSpecificFile/{guid}', data=audio_stream_generator(stream))
+        
+        stop_event.set()  # Stop recording
+        recorder_thread.join()  # Wait for the recording thread to finish
+
+        talkedStream = record_audio(stop_event)  # Get the recorded data
+        
+        guid = str(uuid.uuid4()) 
+        
+        # save on server
+        requests.post(f'{SERVER_URL}/audioToSpecificFile/{guid}', data=talkedStream)
+        requests.post(f'{SERVER_URL}/completeFile/{guid}')
 
 
-
-# Stop and close the stream 
 stream.stop_stream()
 stream.close()
-# Terminate the PortAudio interface
 audio.terminate()
