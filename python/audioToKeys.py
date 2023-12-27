e59887aa7ebb52f30a78a5d48582089cebdc62d0
Neville <live.neville@live.com>
2023-12-27 03:29:45 -0700

    printing only line

--- python/audioToKeys.py
+++ python/audioToKeys.py
@@ -1,9 +1,39 @@
+import pyaudio
+import requests
+import sys
+import uuid
 
 
+# Settings
+SERVER_URL = f"http://{sys.argv[1]}"
+
+# Audio configuration
+FORMAT = pyaudio.paInt16
+CHANNELS = 4
+RATE = 44100
+CHUNK = 1024
+
+
+# Initialize PyAudio
+audio = pyaudio.PyAudio()
+
+# Open stream
+stream = audio.open(format=FORMAT, channels=CHANNELS,
+                    rate=RATE, input=True,
+                    frames_per_buffer=CHUNK)
 
 
 
 with open("keywords.txt", "r") as file:
     for line in file:
-        print(line.strip())  # Remove any trailing newline characters
-        input("Press Enter to continue...")
+        input(line.strip())
+        # input("Press Enter to ...")
+        # requests.post(f'{SERVER_URL}/audioToSpecificFile/{guid}', data=audio_stream_generator(stream))
+
+
+
+# Stop and close the stream 
+stream.stop_stream()
+stream.close()
+# Terminate the PortAudio interface
+audio.terminate()
