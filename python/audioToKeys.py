b2b158e84131366800656f7cd518a2f0390c8d21
Neville <live.neville@live.com>
2023-12-27 18:08:33 -0700

    only file loop

--- python/audioToKeys.py
+++ python/audioToKeys.py
@@ -16,65 +16,61 @@ RATE = 44100
 CHUNK = 1024
 
 
-
-
-def record_audio(stop_event):
-    frames = []
-    while not stop_event.is_set():
-        data = stream.read(CHUNK, exception_on_overflow=False)
-        frames.append(data)
-    return frames
-
-
 def stream_until_seconds(stream: pyaudio.Stream, seconds):
     for _ in range(0, int(RATE / CHUNK * seconds + 1)):
         data = stream.read(CHUNK)
         yield data
         
-def stream_callback(in_data, frame_count, time_info, status):
-    return (in_data, pyaudio.paContinue)
-
-# def stream_until_seconds(stream, seconds):
-#     frames = []
-#     for _ in range(0, int(RATE / CHUNK * seconds)):
-#         data = stream.read(CHUNK)
-#         frames.append(data)
-#     return frames
-
-
 
 audio = pyaudio.PyAudio()
 
 input("Hit enter to begin")
 
 
-with open("keywords.txt", "r") as file:
-    for line in file:
-        line = line.strip();
-        stream = audio.open(format=FORMAT, 
-                            channels=CHANNELS, 
-                            rate=RATE, 
-                            input=True, 
-                            stream_callback=stream_callback)
+def keep_reading_file():
+    with open("keywords.txt", "r") as file:
+        for line in file:
+            line = line.strip();
+
+            key = f"{str(uuid.uuid4())}__{line.replace(' ', '_')}"
+            print(key)
+            input(line)
+
+
+
+keep_reading_file();
+
+
+# with open("keywords.txt", "r") as file:
+#     for line in file:
+#         line = line.strip();
         
-        # start_time = time.time()
-        # input(line.strip())
-        # elapsed_time = time.time() - start_time
 
-        # print(f'gonna take {elapsed_time}')
+
+#         stream = audio.open(format=FORMAT, 
+#                             channels=CHANNELS, 
+#                             rate=RATE, 
+#                             input=True, 
+#                             frames_per_buffer=CHUNK)
         
-        print(line)
+#         # start_time = time.time()
+#         # input(line.strip())
+#         # elapsed_time = time.time() - start_time
 
-        guid = f"{str(uuid.uuid4())}__{line.replace(' ', '_')}"  # str(uuid.uuid4()) 
+#         # print(f'gonna take {elapsed_time}')
         
-        heardData = stream_until_seconds(stream, 3)
+#         print(line)
 
-        # save on server
-        requests.post(f'{SERVER_URL}/audioToSpecificFile/{guid}', data=heardData)
-        requests.post(f'{SERVER_URL}/completeFile/{guid}')
+#         guid = f"{str(uuid.uuid4())}__{line.replace(' ', '_')}"  # str(uuid.uuid4()) 
+        
+#         heardData = stream_until_seconds(stream, 3)
 
-        stream.stop_stream()
-        stream.close()
+#         # save on server
+#         requests.post(f'{SERVER_URL}/audioToSpecificFile/{guid}', data=heardData)
+#         requests.post(f'{SERVER_URL}/completeFile/{guid}')
+
+#         stream.stop_stream()
+#         stream.close()
 
 
 audio.terminate()
