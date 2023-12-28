3328c1130757f0288df55040642b33dbd3019ca1
Neville <live.neville@live.com>
2023-12-27 20:39:20 -0700

    time based

--- python/audioToKeys.py
+++ python/audioToKeys.py
@@ -17,11 +17,10 @@ RATE = 44100
 CHUNK = 1024
 
 
-def stream_until_seconds(stream: pyaudio.Stream, seconds):
+def stream_until_seconds(seconds):
     for _ in range(0, int(RATE / CHUNK * seconds + 1)):
         data = stream.read(CHUNK)
         yield data
-        
 
 audio = pyaudio.PyAudio()
 
@@ -33,81 +32,72 @@ stream = audio.open(format=FORMAT,
                     input=True, 
                     frames_per_buffer=CHUNK)
 
-def readStream():
-    key = CURRENT_KEY
-    while key == CURRENT_KEY: # make sure the key at the starting is still the key
-        data = stream.read(CHUNK, exception_on_overflow=False)
-        yield data
+# def readStream():
+#     key = CURRENT_KEY
+#     while key == CURRENT_KEY: # make sure the key at the starting is still the key
+#         data = stream.read(CHUNK, exception_on_overflow=False)
+#         yield data
 
-def keep_streaming():
-    while CURRENT_KEY != 'DEAD':
-        # data = stream.read(CHUNK, exception_on_overflow=False)
-        if CURRENT_KEY != 'NADA': 
-            requests.post(f'{SERVER_URL}/audioToSpecificFile/{CURRENT_KEY}', data=readStream())
+# def keep_streaming():
+#     while CURRENT_KEY != 'DEAD':
+#         # data = stream.read(CHUNK, exception_on_overflow=False)
+#         if CURRENT_KEY != 'NADA': 
+#             requests.post(f'{SERVER_URL}/audioToSpecificFile/{CURRENT_KEY}', data=readStream())
         
 
-def show_me_the_money():
-    for _ in range(60):
-        requests.get(f'{SERVER_URL}/showthis/{CURRENT_KEY}')
-        # print(f"function 1 says {CURRENT_KEY}")
-        time.sleep(1)
+# def show_me_the_money():
+#     for _ in range(60):
+#         requests.get(f'{SERVER_URL}/showthis/{CURRENT_KEY}')
+#         # print(f"function 1 says {CURRENT_KEY}")
+#         time.sleep(1)
 
-def keep_reading_file():
-    global CURRENT_KEY
-    while True:
-        with open("keywords.txt", "r") as file:
-            for line in file:
-                line = line.strip();
-                curr = CURRENT_KEY = f"{str(uuid.uuid4())}__{line.replace(' ', '_')}"
-                input(line)
-                CURRENT_KEY = 'NADA'
-                requests.post(f'{SERVER_URL}/completeFile/{curr}')
+# def keep_reading_file():
+#     global CURRENT_KEY
+#     while True:
+#         with open("keywords.txt", "r") as file:
+#             for line in file:
+#                 line = line.strip();
+#                 curr = CURRENT_KEY = f"{str(uuid.uuid4())}__{line.replace(' ', '_')}"
+#                 input(line)
+#                 CURRENT_KEY = 'NADA'
+#                 requests.post(f'{SERVER_URL}/completeFile/{curr}')
             
 
-    CURRENT_KEY = 'DEAD'
+#     CURRENT_KEY = 'DEAD'
 
 
-fileReadThread = threading.Thread(target=keep_reading_file)
-streamThread = threading.Thread(target=keep_streaming)
+# fileReadThread = threading.Thread(target=keep_reading_file)
+# streamThread = threading.Thread(target=keep_streaming)
 
-fileReadThread.start()
-streamThread.start()
+# fileReadThread.start()
+# streamThread.start()
 
-fileReadThread.join()
-streamThread.join()
+# fileReadThread.join()
+# streamThread.join()
 
 print('yah we done here')
 
-# with open("keywords.txt", "r") as file:
-#     for line in file:
-#         line = line.strip();
+with open("keywords.txt", "r") as file:
+    for line in file:
         
-
-
-#         stream = audio.open(format=FORMAT, 
-#                             channels=CHANNELS, 
-#                             rate=RATE, 
-#                             input=True, 
-#                             frames_per_buffer=CHUNK)
+        items = line.strip().split(', ')
         
-#         # start_time = time.time()
-#         # input(line.strip())
-#         # elapsed_time = time.time() - start_time
-
-#         # print(f'gonna take {elapsed_time}')
+        key = items[0]
+        seconds = 4
         
-#         print(line)
-
-#         guid = f"{str(uuid.uuid4())}__{line.replace(' ', '_')}"  # str(uuid.uuid4()) 
+        if (items.count() > 1):
+            seconds = items[1]
         
-#         heardData = stream_until_seconds(stream, 3)
+        print(key)
 
-#         # save on server
-#         requests.post(f'{SERVER_URL}/audioToSpecificFile/{guid}', data=heardData)
-#         requests.post(f'{SERVER_URL}/completeFile/{guid}')
+        guid = f"{str(uuid.uuid4())}__{key.replace(' ', '_')}"  # str(uuid.uuid4()) 
+        
+        # heardData = stream_until_seconds(stream, 3)
 
-#         stream.stop_stream()
-#         stream.close()
+        # save on server
+        requests.post(f'{SERVER_URL}/audioToSpecificFile/{guid}', data=stream_until_seconds(seconds))
+        requests.post(f'{SERVER_URL}/completeFile/{guid}')
+
 
 
 stream.stop_stream()
