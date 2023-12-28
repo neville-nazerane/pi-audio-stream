cdac4aee60bbad15dd8076b6cc071e2d87320913
Neville <live.neville@live.com>
2023-12-27 18:31:20 -0700

    blobal on root

--- python/audioToKeys.py
+++ python/audioToKeys.py
@@ -33,10 +33,10 @@ def show_me_the_money():
         time.sleep(1)
 
 def keep_reading_file():
+    global CURRENT_KEY
     with open("keywords.txt", "r") as file:
         for line in file:
             line = line.strip();
-            global CURRENT_KEY
             CURRENT_KEY = f"{str(uuid.uuid4())}__{line.replace(' ', '_')}"
             input(line)
 
