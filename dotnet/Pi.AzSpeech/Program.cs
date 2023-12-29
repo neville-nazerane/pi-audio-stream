22e6e575b128a5427c868b6aec2116a2e3e7acf9
Neville <live.neville@live.com>
2023-12-29 15:23:14 -0700

    the loop and fingers crossed

--- dotnet/Pi.AzSpeech/Program.cs
+++ dotnet/Pi.AzSpeech/Program.cs
@@ -1,11 +1,36 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using Microsoft.Extensions.Configuration;
 
+#region Configs
 
-Console.WriteLine("Running");
 
 var configs = new ConfigurationBuilder()
-                        .AddUserSecrets("pi azSpeech")
-                        .Build();
+                .AddUserSecrets("pi azSpeech")
+                .Build();
 
 
-Console.WriteLine($"Setting is {configs["test"]}");
+
+var speechConfig = SpeechConfig.FromSubscription(configs["key"],
+                                                 configs["region"]);
+speechConfig.SpeechRecognitionLanguage = "en-US";
+
+
+#endregion
+
+
+
+using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+using var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+
+while (true)
+{
+    Console.WriteLine("Speak now or forever hold your chickpeas");
+
+    var result = await speechRecognizer.RecognizeOnceAsync();
+
+    Console.WriteLine($"Time taken: {result.Duration.TotalSeconds}");
+    Console.WriteLine($"Detected: {result.Text}");
+    Console.WriteLine($"Reason: {result.Reason}");
+}
