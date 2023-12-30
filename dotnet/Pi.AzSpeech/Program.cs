a92036c4a059c5d382f3af3199e6cadeb6df755c
Neville <live.neville@live.com>
2023-12-29 21:33:06 -0700

    task delay

--- dotnet/Pi.AzSpeech/Program.cs
+++ dotnet/Pi.AzSpeech/Program.cs
@@ -38,9 +38,12 @@ using var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
 speechRecognizer.Recognized += SpeechRecognizer_Recognized;
 await speechRecognizer.StartKeywordRecognitionAsync(keywordModel);
 
-await speechRecognizer.StartContinuousRecognitionAsync();
-
 Console.WriteLine("Alright.... listening now...");
+
+await Task.Delay(6000);
+
+//await speechRecognizer.StartContinuousRecognitionAsync();
+
 
 void SpeechRecognizer_Recognized(object? sender, SpeechRecognitionEventArgs e)
 {
