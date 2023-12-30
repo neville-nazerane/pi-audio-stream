9b2b832f9b611f1a62c3d97f5fbd1d93ae411cf6
Neville <live.neville@live.com>
2023-12-29 22:51:53 -0700

    lights

---
+++ python/rgb.py
@@ -0,0 +1,13 @@
+import time
+from respeaker import PixelRing
+
+pixel_ring = PixelRing()  # Initialize the pixel ring
+pixel_ring.set_brightness(10)  # Set brightness
+
+# Set the LEDs to red
+pixel_ring.set_color(r=255, g=0, b=0)
+
+time.sleep(3)  # Keep the color for 3 seconds
+
+# Turn off the LEDs
+pixel_ring.off()
