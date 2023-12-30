7c61aa4ff9fdbba2bc7562daa101e022d3e1dc00
Neville <live.neville@live.com>
2023-12-29 17:44:40 -0700

    keywords!

--- dotnet/Pi.AzSpeech/Program.cs
+++ dotnet/Pi.AzSpeech/Program.cs
@@ -11,27 +11,37 @@ var configs = new ConfigurationBuilder()
 
 
 
-var speechConfig = SpeechTranslationConfig.FromSubscription(configs["key"],
+var speechConfig = SpeechConfig.FromSubscription(configs["key"],
                                                  configs["region"]);
 
 
 speechConfig.SpeechRecognitionLanguage = "en-US";
-speechConfig.AddTargetLanguage("hi-IN");
 
 #endregion
 
+using var keywordModel = KeywordRecognitionModel.FromFile("ML_Models");
 using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+
+using var keywordRecognizer = new KeywordRecognizer(audioConfig);
+
+
 using var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
 
 
 while (true)
 {
-    Console.WriteLine("\n\n\n\n\n\n\n\nSpeak now or forever hold your chickpeas");
-
-    var result = await speechRecognizer.RecognizeOnceAsync();
-
-    Console.WriteLine($"\n\n\nTime taken: {result.Duration.TotalSeconds}");
-    Console.WriteLine($"Detected: {result.Text}");
-    Console.WriteLine($"Reason: {result.Reason}");
-
+    await keywordRecognizer.RecognizeOnceAsync(keywordModel);
+    Console.WriteLine("Oh hello there");
 }
+
+//while (true)
+//{
+//    Console.WriteLine("\n\n\n\n\n\n\n\nSpeak now or forever hold your chickpeas");
+
+//    var result = await speechRecognizer.RecognizeOnceAsync();
+
+//    Console.WriteLine($"\n\n\nTime taken: {result.Duration.TotalSeconds}");
+//    Console.WriteLine($"Detected: {result.Text}");
+//    Console.WriteLine($"Reason: {result.Reason}");
+
+//}
