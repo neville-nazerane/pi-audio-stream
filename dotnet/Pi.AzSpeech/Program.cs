1084d8d79af500b5e9adc5a3cbca8d65bb9ee26c
Neville <live.neville@live.com>
2023-12-29 18:14:23 -0700

    patching keywords

--- dotnet/Pi.AzSpeech/Program.cs
+++ dotnet/Pi.AzSpeech/Program.cs
@@ -19,30 +19,43 @@ speechConfig.SpeechRecognitionLanguage = "en-US";
 
 #endregion
 
-using var keywordModel = KeywordRecognitionModel.FromFile("ML_Models");
+var modelPath = Path.Combine(GetCurrentFolder(),
+                             "ML_Models",
+                             "keyword.table");
+
+using var keywordModel = KeywordRecognitionModel.FromFile(modelPath);
 using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
 
-//using var keywordRecognizer = new KeywordRecognizer(audioConfig);
+using var keywordRecognizer = new KeywordRecognizer(audioConfig);
 
 
 using var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
 
 Console.WriteLine("SAY IT!");
 
-//while (true)
-//{
-//    await keywordRecognizer.RecognizeOnceAsync(keywordModel);
-//    Console.WriteLine("Oh hello there");
-//}
-
 while (true)
 {
-    Console.WriteLine("\n\n\n\n\n\n\n\nSpeak now or forever hold your chickpeas");
-
-    var result = await speechRecognizer.RecognizeOnceAsync();
-
-    Console.WriteLine($"\n\n\nTime taken: {result.Duration.TotalSeconds}");
-    Console.WriteLine($"Detected: {result.Text}");
-    Console.WriteLine($"Reason: {result.Reason}");
-
+    await keywordRecognizer.RecognizeOnceAsync(keywordModel);
+    Console.WriteLine("Oh hello there");
 }
+
+//while (true)
+//{
+//    Console.WriteLine("\n\n\n\n\n\n\n\nSpeak now or forever hold your chickpeas");
+
+//    var result = await speechRecognizer.RecognizeOnceAsync();
+
+//    Console.WriteLine($"\n\n\nTime taken: {result.Duration.TotalSeconds}");
+//    Console.WriteLine($"Detected: {result.Text}");
+//    Console.WriteLine($"Reason: {result.Reason}");
+
+//}
+
+
+
+
+
+static string GetCurrentFolder() 
+    => Directory.GetCurrentDirectory()
+                       .Split(["/bin/", "/bin/"], StringSplitOptions.None)
+                       .First();
