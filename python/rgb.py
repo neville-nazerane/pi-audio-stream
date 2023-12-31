5baa0425d6078135af4b47ac65708dffe2a7a5a1
Neville <live.neville@live.com>
2023-12-31 15:21:20 -0700

    using ring updated

--- python/rgb.py
+++ python/rgb.py
@@ -16,10 +16,14 @@
 import time
 
 from pixel_ring import pixel_ring
+from gpiozero import LED
 
+power = LED(5)
+power.on()
+
+pixel_ring.set_brightness(10)
 
 if __name__ == '__main__':
-    pixel_ring.change_pattern('echo')
     while True:
 
         try:
@@ -37,3 +41,5 @@ if __name__ == '__main__':
 
     pixel_ring.off()
     time.sleep(1)
+
+power.off()
