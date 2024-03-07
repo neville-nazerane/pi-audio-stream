43e976d2cc74db82fcc48843cf71019439206fab
Neville <live.neville@live.com>
2024-03-06 21:05:51 -0700

    listing devices

--- dotnet/Pi.SpeechStream/Program.cs
+++ dotnet/Pi.SpeechStream/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Diagnostics;
+
+
+ListAudioDevices();
+
+
+
+static void ListAudioDevices()
+{
+    ProcessStartInfo psi = new ProcessStartInfo
+    {
+        FileName = "arecord",
+        Arguments = "-l", // Lists the recording sound devices
+        UseShellExecute = false,
+        RedirectStandardOutput = true,
+        CreateNoWindow = true
+    };
+
+    try
+    {
+        using (Process process = Process.Start(psi))
+        {
+            using (System.IO.StreamReader reader = process.StandardOutput)
+            {
+                string result = reader.ReadToEnd();
+                Console.Write(result);
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"An error occurred: {ex.Message}");
+    }
+}
